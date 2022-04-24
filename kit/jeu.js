150060d95542d4f1d9694d57de5ef480738742d1
Laura <laura2004arust@gmail.com>
2022-04-24 21:50:49 +0200

    CSS

--- kit/jeu.js
+++ kit/jeu.js
@@ -7,6 +7,17 @@ Prénom :
 
 */
 
-function alea(min, max){ // [0;15[
-  return Math.floor((Math.random()*(max-min))+min)
+function alea(min, max) { // [0;15[
+  return Math.floor((Math.random() * (max - min)) + min)
+}
+
+for (var i = 0; i < 16; i++) {
+  console.log(i);
+  var row = Math.floor(i/4); 
+  var col = i%4;
+  var tile = $("<div id=\"tile"+i+"\"> <p>"+i+"</p></div>").addClass("tuile");
+  tile.css({top: 90*row, left: 90*col});
+  if(i<15)
+  tile.css({"background-image":"url('/kit/img/0"+i+".jpg')"});
+  $("#puzzlearea").append(tile);
 }
